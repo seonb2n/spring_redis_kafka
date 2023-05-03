b1a030ccb9fcf156d1569b9992f8347c38ecafec
seonb2n <626ksb@gmail.com>
2023-05-03 21:55:30 +0900

    [FEATURE] - Service 명세 정의

---
+++ src/main/java/com/example/spring_redis_kafka/service/LowestPriceService.java
@@ -0,0 +1,25 @@
+package com.example.spring_redis_kafka.service;
+
+
+import com.example.spring_redis_kafka.vo.Keyword;
+import com.example.spring_redis_kafka.vo.Product;
+import com.example.spring_redis_kafka.vo.ProductGroup;
+
+import java.util.Set;
+
+public interface LowestPriceService {
+    Set GetZsetValue(String key);
+
+    Set GetZsetValueWithStatus(String key) throws Exception;
+
+    Set GetZsetValueWithSpecificException(String key) throws Exception;
+
+    int SetNewProduct(Product newProduct);
+
+    int SetNewProductGrp(ProductGroup newProductGrp);
+
+    int SetNewProductGrpToKeyword(String keyword, String prodGrpId, double score);
+
+    Keyword GetLowestPriceProductByKeyword(String keyword);
+
+}
