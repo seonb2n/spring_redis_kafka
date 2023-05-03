629027bd2f7d729ed76c73a64a04eba0f974908b
seonb2n <626ksb@gmail.com>
2023-05-03 21:30:30 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/spring_redis_kafka/SpringRedisKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_redis_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_redis_kafka/SpringRedisKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_redis_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
